02d62c55d41cc65007b71075dd238e287ac2f487
Piter Kubo Chagas <kubotec10@gmail.com>
2026-01-06 17:51:37 -0300

    Criando thread no servidorSockets

--- Projeto/Modulo - 1 Fundamentos do Java/Cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
+++ Projeto/Modulo - 1 Fundamentos do Java/Cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -1,6 +1,7 @@
 package mx.florinda.cardapio;
 
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -8,6 +9,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import javax.management.RuntimeErrorException;
 
 public class ServidorItensCardapioComSocket {
     
@@ -26,63 +29,80 @@ public class ServidorItensCardapioComSocket {
             while(true){
             
                 //todas vez que vem uma requisição do cliente é gerado um clientSocket
-                try(Socket clientSocket = serverSocket.accept()){
-
-                    // verifica o que o cliente escreveu
-                    InputStream clienteIS = clientSocket.getInputStream();
-
-                    StringBuilder requestBuilder = new StringBuilder();
-                    
-                    
-                    int data;
-
-                    do{
-                        data = clienteIS.read();
-                        requestBuilder.append((char)data);
-                    }
-
-                    while(clienteIS.available() > 0);
-                    
-                    String request = requestBuilder.toString();
-
-                    IO.println(request);
-
-               
+                Socket clientSocket = serverSocket.accept();
+                // declarando a thread
+                Thread thread = new Thread(() -> TrataRequisicao(clientSocket));
+                thread.start();
 
                     
+                      
+                        
+            }
+              
 
-                    // Enviar o response para o cliente via json
-
-
-                    // pegando path
-                    Path path = Path.of("itensCardapio.json");
-                    
-
-                    //lendo o arquivo json
-                    String json =  Files.readString(path);
-
-
-                    OutputStream clienteOS = clientSocket.getOutputStream();
-                    
-                    PrintStream clientOut = new PrintStream(clienteOS);
-
-                    // Criando o cabeçalho 
-                    clientOut.println("HTTP/1.1 200 OK");                
-                    clientOut.println("Content-type: application/json; charset=UTF-8");
-                    clientOut.println();
-                    clientOut.println(json);
-                };
+        }
          
+
+    }
+
+    private static void TrataRequisicao(Socket clientSocket) {
+        
+        // verifica o que o cliente escreveu
+        try(clientSocket){
+
             
-            
-            
+            InputStream clienteIS = clientSocket.getInputStream();
+
+            StringBuilder requestBuilder = new StringBuilder();
+        
+        
+            int data;
+
+            do{
+                data = clienteIS.read();
+                requestBuilder.append((char)data);
             }
 
-                
+            while(clienteIS.available() > 0);
+        
+            String request = requestBuilder.toString();
+
+            IO.println(request);
+
+            Thread.sleep(250);          
+        
+
+            // Enviar o response para o cliente via json
+
+
+            // pegando path
+            Path path = Path.of("itensCardapio.json");
+        
+
+             //lendo o arquivo json
+            String json =  Files.readString(path);
+
+
+            OutputStream clienteOS = clientSocket.getOutputStream();
+        
+            PrintStream clientOut = new PrintStream(clienteOS);
+
+            // Criando o cabeçalho 
+            clientOut.println("HTTP/1.1 200 OK");                
+            clientOut.println("Content-type: application/json; charset=UTF-8");
+            clientOut.println();
+            clientOut.println(json);
+
+
+
+
 
         }
 
-          
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
 
     }
 
