0efaf55f7dce4b0b82d2740a09424bbcc386dccb
Piter Kubo Chagas <kubotec10@gmail.com>
2026-01-15 20:05:46 -0300

    Implementando liveness, readness, timeout, circuit break

---
+++ Projeto/Modulo - 2 Desenvolvimento de Aplicações Back-End com Spring boot e quarkus/code-with-quarkus/src/main/java/com/kubo/LivenessCheck.java
@@ -0,0 +1,16 @@
+package com.kubo;
+
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class LivenessCheck implements HealthCheck {
+
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Liveness check" ) ;
+    }
+}
