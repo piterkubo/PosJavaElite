2fa925b8caf6f6a6b0f1dbac46e5194d0071825f
Piter Kubo Chagas <71854896+piterkubo@users.noreply.github.com>
2026-01-20 18:34:15 -0300

    1 - Aula de spring + projetos

---
+++ Projeto/Modulo - 2 Desenvolvimento de Aplicações Back-End com Spring boot e quarkus/2_spring_helloworld/src/main/java/br/com/kubo/helloworld/controller/HelloController.java
@@ -0,0 +1,40 @@
+package br.com.kubo.helloworld.controller;
+
+import br.com.kubo.helloworld.model.Produto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // declaração para requisição web
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+
+    @GetMapping("/produtos")
+    public Produto getProduto()
+    {
+        Produto p =  new Produto();
+        p.setId(12345);
+        p.setNome("Computador");
+        p.setPreco(1500.0);
+
+        return p;
+    }
+
+
+    @PostMapping("/produtos")
+    public String addNewProdutc(@RequestBody Produto p){
+        IO.println("Produto Recebido");
+        IO.println(p.getId() + "-" + p.getNome() + "-" + p.getPreco());
+        return "OK";
+
+    }
+
+
+}
