0a071755167e1eb6c28787f67a2d843388d37ddb
piter kubo Chagas <kubotec10@gmail.com>
2026-02-03 17:35:14 -0300

    Criando o primeiro projeto de agencia de viagem com IA

---
+++ Projeto/agencia-viagem-ia/src/main/java/dev/ia/TavelAgentAssistant.java
@@ -0,0 +1,22 @@
+package dev.ia;
+
+
+// Esta anotação instrui o Quarkus a gerar uma implementaçaão desta interface que se conecta
+
+import io.quarkiverse.langchain4j.RegisterAiService;
+
+@RegisterAiService
+public interface TavelAgentAssistant {
+
+    /**
+     * O método 'chat' recebe a mensagem do usuario e retorna a resposta do LLM.
+     * @param userMessage A mensagem do usuario.
+     * @return A resposta gerada pelo modelo da linguagem.
+     *
+    */
+
+    String chat(String userMessage);
+
+
+
+}
