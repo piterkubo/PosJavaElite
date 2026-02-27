08d5199c9ac89154b77b9e11d9ccc86151a9f1c9
piter kubo Chagas <kubotec10@gmail.com>
2026-02-27 18:32:22 -0300

    Aula 5 part 1 criando o projeto

---
+++ Projeto/Modulo 3  Fundamentos Front-end com react/MinAppCompleto/projetos/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div>
+     <h1>Bem Vindo !</h1>
+     <p>Telas Disponiveis</p>
+     
+     <ul>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+     </ul>
+    </div>
+  );
+}
